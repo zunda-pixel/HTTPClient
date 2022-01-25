17de353c653026e6189e898c3e6b2e4df9a32320
zunda-pixel <47569369+zunda-pixel@users.noreply.github.com>
2022-01-25 10:02:05 +0900

    iOS 13, iOS 14でも利用できるようにExtensionを用意

---
+++ Sources/HTTPClient/URLSessionExtension.swift
@@ -0,0 +1,39 @@
+//
+//  URLSessionExtension.swift
+//  
+//
+//  Created by zunda on 2022/01/24.
+//
+
+import Foundation
+
+@available(iOS, introduced: 13.0, deprecated: 15.0, message: "Use the built-in API instead")
+extension URLSession {
+  func data(from url: URL) async throws -> (Data, URLResponse) {
+    try await withCheckedThrowingContinuation { continuation in
+      self.dataTask(with: url) { data, response, error in
+        if let error = error {
+          return continuation.resume(throwing: error)
+        }
+        guard let data = data, let response = response else {
+          return continuation.resume(throwing: URLError(.badServerResponse))
+        }
+        continuation.resume(returning: (data, response))
+      }.resume()
+    }
+  }
+  
+  func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+    try await withCheckedThrowingContinuation { continuation in
+      self.dataTask(with: request) { data, response, error in
+        if let error = error {
+          return continuation.resume(throwing: error)
+        }
+        guard let data = data, let response = response else {
+          return continuation.resume(throwing: URLError(.badServerResponse))
+        }
+        continuation.resume(returning: (data, response))
+      }.resume()
+    }
+  }
+}

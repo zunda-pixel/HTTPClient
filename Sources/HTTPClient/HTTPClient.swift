777a3fbe34179cb92064a6180a41a51ec187e2c9
zunda-pixel <47569369+zunda-pixel@users.noreply.github.com>
2022-01-25 10:01:36 +0900

    HTTPClientを実装

--- Sources/HTTPClient/HTTPClient.swift
+++ Sources/HTTPClient/HTTPClient.swift
@@ -1,6 +1,66 @@
-public struct HTTPClient {
-    public private(set) var text = "Hello, World!"
+//
+//  HTTPClient.swift
+//
+//
+//  Created by zunda on 2022/01/24.
+//
 
-    public init() {
-    }
+import Foundation
+import HTTPMethod
+
+#if os(Linux) || os(Windows)
+import FoundationNetworking
+#endif
+
+public struct HTTPClient {
+  public static let timeout: Double = 60.0
+  
+  public static func request(method: HTTPMethod, url: URL, queries: [String: String?] = [:], headers: [String: String] = [:], body: Data? = nil,  timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+    urlComponents.queryItems = queries.map { .init(name: $0, value: $1)}
+    
+    var request: URLRequest = .init(url: urlComponents.url!)
+    request.httpMethod = method.rawValue
+    request.allHTTPHeaderFields = headers
+    request.httpBody = body
+    request.timeoutInterval = timeout
+    
+    return try await URLSession.shared.data(for: request)
+  }
+  
+  public static func get(url: URL, body: Data? = nil, headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .get, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
+  }
+  
+  public static func post(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .post, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
+  }
+  
+  public static func put(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .put, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
+  }
+  
+  public static func delete(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .trace, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
+  }
+  
+  public static func connect(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .connect, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
+  }
+  
+  public static func head(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .head, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
+  }
+  
+  public static func options(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .options, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
+  }
+  
+  public static func patch(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .patch, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
+  }
+  
+  public static func trace(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .trace, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
+  }
 }

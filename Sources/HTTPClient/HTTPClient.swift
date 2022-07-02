eb58ad2d997f7e838c08523eccb3d41deae66ca6
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-07-03 08:47:46 +0900

    URLSession.Sharedではなく、インスタンス変数を使用するように

--- Sources/HTTPClient/HTTPClient.swift
+++ Sources/HTTPClient/HTTPClient.swift
@@ -15,7 +15,7 @@ import FoundationNetworking
 extension URLSession {
   public static let timeout: Double = 60.0
   
-  public static func request(method: HTTPMethod, url: URL, queries: [String: String?] = [:], headers: [String: String] = [:], body: Data? = nil,  timeout: Double = timeout) async throws -> (Data, URLResponse) {
+  public func request(method: HTTPMethod, url: URL, queries: [String: String?] = [:], headers: [String: String] = [:], body: Data? = nil,  timeout: Double = timeout) async throws -> (Data, URLResponse) {
     var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)!
 
     if !queries.isEmpty {
@@ -28,42 +28,42 @@ extension URLSession {
     request.httpBody = body
     request.timeoutInterval = timeout
     
-    return try await URLSession.shared.data(for: request)
+    return try await data(for: request)
   }
   
-  public static func get(url: URL, body: Data? = nil, headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+  public func get(url: URL, body: Data? = nil, headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
     return try await request(method: .get, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
   }
   
-  public static func post(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+  public func post(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
     return try await request(method: .post, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
   }
   
-  public static func put(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+  public func put(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
     return try await request(method: .put, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
   }
   
-  public static func delete(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+  public func delete(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
     return try await request(method: .delete, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
   }
   
-  public static func connect(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+  public func connect(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
     return try await request(method: .connect, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
   }
   
-  public static func head(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+  public func head(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
     return try await request(method: .head, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
   }
   
-  public static func options(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+  public func options(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
     return try await request(method: .options, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
   }
   
-  public static func patch(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+  public func patch(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
     return try await request(method: .patch, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
   }
   
-  public static func trace(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+  public func trace(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String?] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
     return try await request(method: .trace, url: url, queries: queries, headers: headers, body: body, timeout: timeout)
   }
 }
